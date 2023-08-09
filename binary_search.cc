593141836021dc4b24873e5b49cc93d8965aabd4
Sarah Wilson <106021956+sarah-wilsxn@users.noreply.github.com>
2023-08-09 00:40:36 -0400

    Create binary_search.cc

---
+++ binary_search.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty()){
+            return 0;
+        } else if (target > nums[nums.size() - 1]){
+            return -1;
+        } else if (target == nums[nums.size() - 1]){
+            return nums.size() - 1;
+        }
+        int l = 0;
+        int r = nums.size() - 1;
+        for (int i = 0; i < nums.size(); i++){
+            int m = (l + r) / 2;
+            if (target == nums[m]){
+                return m;
+            } else if (target > nums[m]){
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+};

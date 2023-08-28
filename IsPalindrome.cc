c7db208edfa51a871a6983f9ffc41fa380d2e31e
Sarah Wilson <106021956+sarah-wilsxn@users.noreply.github.com>
2023-08-28 03:16:20 -0400

    Update IsPalindrome.cc

--- IsPalindrome.cc
+++ IsPalindrome.cc
@@ -1,5 +1,3 @@
-// wildly inefficient but I enjoy stacks
-
 class Solution {
 public:
     bool isPalindrome(string s) {
@@ -11,28 +9,12 @@ public:
             }
         }
         if (ans.length() == 1) return true;
-        else (ans.length() == 2 && ans[0] == ans[1]) ? true : false;
-        int n = ans.length();
-        stack<char> s1, s2;
-        if (n % 2 == 0){ // even
-            for (int i = 0; i < n / 2; i++){
-                s1.push(ans[i]);
-            }
-            for (int i = n - 1; i >= n / 2; i--){
-                s2.push(ans[i]);
-            }
-        } else {
-            for (int i = 0; i < n / 2; i++){
-                s1.push(ans[i]);
-            }
-            for (int i = n - 1; i > n / 2; i--){
-                s2.push(ans[i]);
-            }
-        }
-        while (!s1.empty()){
-            if (s1.top() != s2.top()) return false;
-            s1.pop(); 
-            s2.pop();
+        int start = 0;
+        int end = ans.length() - 1;
+        while (start <= end){
+            if (ans[start] != ans[end]) return false;
+            start++;
+            end--;
         }
         return true;
     }

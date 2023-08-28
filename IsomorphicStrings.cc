d4ee7c650139fd64a9d402e72cdac48a2fe07847
Sarah Wilson <106021956+sarah-wilsxn@users.noreply.github.com>
2023-08-28 02:40:14 -0400

    Update IsomorphicStrings.cc

--- IsomorphicStrings.cc
+++ IsomorphicStrings.cc
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> m, m1;
+    bool helper(string s, string t) {
+        unordered_map<char, char> m;
         for (int i = 0; i < s.length(); i++) {
             if (m[s[i]] && m[s[i]] != t[i]) return false;
-            if (m1[t[i]] && m1[t[i]] != s[i]) return false;
             m[s[i]] = t[i];
-            m1[t[i]] = s[i];
         }
         return true;
+    }
+
+    bool isIsomorphic(string s, string t) {
+        return (helper(s, t) && helper(t, s));
     }
 };
